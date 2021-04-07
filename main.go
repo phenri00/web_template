b8a69cbbf32b16435c8186dcc12486d17d51b412
Patrick Henriksson <patrick.henriksson@gmail.com>
2021-04-07 15:09:27 +0200

    feat
    added templating

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 )
 
 type App struct {
@@ -13,6 +14,7 @@ type App struct {
 
 func (a *App) Start() {
 	addr := fmt.Sprintf(":%s", a.Port)
+	http.Handle("/", logreq(index))
 	log.Printf("Starting app on %s", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
@@ -23,6 +25,35 @@ func env(key, defaultValue string) string {
 		return defaultValue
 	}
 	return val
+}
+
+func logreq(f func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("path: %s", r.URL.Path)
+		f(w, r)
+	})
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index.html", struct {
+		Name string
+	}{
+		Name: "Sonic the Hedgehog",
+	})
+}
+
+func renderTemplate(w http.ResponseWriter, name string, data interface{}) {
+	t, err := template.ParseGlob("templates/*.html")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error %s", err.Error()), 500)
+		return
+	}
+
+	err = t.ExecuteTemplate(w, name, data)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error %s", err.Error()), 500)
+		return
+	}
 }
 
 func main() {
